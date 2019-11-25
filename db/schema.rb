40de89638d45ecc071b0e59a833f3364b7524f87
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-25 17:53:24 +0100

    Generate Rental, Rent, Incident

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_153246) do
+ActiveRecord::Schema.define(version: 2019_11_25_164334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "address"
+    t.text "description"
+    t.integer "nb_rooms", default: 1
+    t.integer "area"
+    t.boolean "furnished", default: false
+    t.string "syndic_name"
+    t.string "syndic_phone"
+    t.string "syndic_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
+
+  create_table "incidents", force: :cascade do |t|
+    t.bigint "flat_id"
+    t.date "date"
+    t.string "status", default: "pending"
+    t.string "dispo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_incidents_on_flat_id"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "flat_id"
+    t.date "start_date"
+    t.integer "duration", default: 3
+    t.integer "rent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_rentals_on_flat_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
+  create_table "rents", force: :cascade do |t|
+    t.bigint "rental_id"
+    t.string "status", default: "pending"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rental_id"], name: "index_rents_on_rental_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +77,8 @@ ActiveRecord::Schema.define(version: 2019_11_25_153246) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flats", "users"
+  add_foreign_key "incidents", "flats"
+  add_foreign_key "rentals", "flats"
+  add_foreign_key "rentals", "users"
 end
