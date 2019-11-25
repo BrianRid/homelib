546c9255f1a2d49c35e539ce77d94364cacf6edc
Theodore JUDLIN <theodore.judlin@free.fr>
2019-11-25 18:12:44 +0100

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_153246) do
+ActiveRecord::Schema.define(version: 2019_11_25_161650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "address"
+    t.text "description"
+    t.integer "nb_rooms", default: 1
+    t.integer "area"
+    t.boolean "furnished", default: false
+    t.string "syndic_name"
+    t.string "syndic_phone"
+    t.string "syndic_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +46,5 @@ ActiveRecord::Schema.define(version: 2019_11_25_153246) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flats", "users"
 end
