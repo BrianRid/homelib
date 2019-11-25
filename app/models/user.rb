381a9d98ffdba388f38f23e2cea989f5bd03722f
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-25 17:25:26 +0100

    Model User + Model Flat

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
