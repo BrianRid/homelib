160a2370a24e24573d58348d5d1945f1d33eb45c
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-26 18:04:50 +0100

    make an avatar with initial if user haven't upload photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :avatar
   has_many :user_workers
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
+  def initial
+    "#{first_name.first.capitalize}#{last_name.first.capitalize}"
   end
 end
