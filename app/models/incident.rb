e7b339277755b55cd54e0d55315c417c68f19455
Theodore JUDLIN <theodore.judlin@free.fr>
2019-11-26 15:40:16 +0100

    incidents

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,6 +1,5 @@
 class Incident < ApplicationRecord
   belongs_to :flat
-  has_many :incidents
   has_many :works
   validates :dispo, presence: true
   validates :category, presence: true, inclusion: { in: ["plomberie", "électricité", "serrurerie", "chauffage", "vitrerie", "petits travaux", "jardinage", "peinture", "sols", "électroménager"]}
