e406620a4e34e3d2abe65819fa78f9ffb06567b9
Theodore JUDLIN <theodore.judlin@free.fr>
2019-11-26 14:10:27 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,8 @@ puts 'Cleaning database...'
 # puts 'all userworkers destroyed'
 # Rentals.destroy_all
 # puts 'all rentals destroyed'
-# Incidents.destroy_all
-# puts 'all incidents destroyed'
+Incident.destroy_all
+puts 'all incidents destroyed'
 Flat.destroy_all
 puts 'all flats destroyed'
 User.destroy_all
@@ -27,7 +27,7 @@ puts 'all users destroyed'
 puts 'all records destroyed'
 puts 'Creating some of each...'
 
-
+puts 'Creating users...'
 rock = User.create!({
   email:  "rf@gmail.com",
   password: "123456",
@@ -55,7 +55,7 @@ robert = User.create!({
   phone: "06 12 64 34 22",
 })
 
-
+puts 'Creating flats...'
 f1 = Flat.create!({
   user_id: rock.id,
   address: "35 avenue Mac Mahon",
@@ -63,11 +63,114 @@ f1 = Flat.create!({
   nb_rooms: 2,
   area: "64 m2",
   furnished: false,
-  syndic_name: "",
-  syndic_phone: "",
-  syndic_email: "",
+  syndic_name: "Michau - Mme Noel",
+  syndic_phone: "01 45 43 56 76",
+  syndic_email: "contact@foncia.fr",
 })
 
+f2 = Flat.create!({
+  user_id: rock.id,
+  address: "35 rue du Loup",
+  description: "Beaucoup de potentiel, orienté nord, moulure, chauffage central",
+  nb_rooms: 2,
+  area: "26 m2",
+  furnished: false,
+  syndic_name: "Foncia - Mme Durand",
+  syndic_phone: "01 45 43 56 76",
+  syndic_email: "contact@foncia.fr",
+})
+
+puts 'Creating incidents...'
+i1 = Incident.create!({
+  flat_id: f1.id,
+  date: Date.new(2019,12,15),
+  status: "finalisé",
+  dispo: "matin tôt",
+  # type: "fuite",
+})
+
+i2 = Incident.create!({
+  flat_id: f1.id,
+  date: Date.new(2019,12,15),
+  status: "finalisé",
+  dispo: "matin tôt",
+  # type: "fuite",
+})
+
+i3 = Incident.create!({
+  flat_id: f2.id,
+  date: Date.new(2015,7,11),
+  status: "finalisé",
+  dispo: "matin tôt",
+  # type: "fuite",
+})
+
+i4 = Incident.create!({
+  flat_id: f2.id,
+  date: Date.new(2017,4,22),
+  status: "finalisé",
+  dispo: "soir tard",
+  # type: "fuite",
+})
+
+puts 'Creating rantal 1 AND RENTS...'
+r1 = Rental.create!({
+  user_id: alain.id,
+  flat_id: f1.id,
+  start_date: Date.new(2015,11,01),
+  duration: 3,
+  rent: 1770,
+})
+d1 = r1.start_date
+until d1 = Date.new(2019, DateTime.now().month - 1, 01) do
+  d1 += 1.month
+  Rent.create!({
+    rental_id: r1.id,
+    date: d1
+  })
+end
+
+puts 'Creating rantal 2 AND RENTS...'
+r2 = Rental.create!({
+  user_id: robert.id,
+  flat_id: f2.id,
+  start_date: Date.new(2018,8,01),
+  duration: 3,
+  rent: 970,
+})
+d2 = r2.start_date
+until d2 = Date.new(2019, DateTime.now().month - 1, 01) do
+  d1 += 1.month
+  Rent.create!({
+    rental_id: r2.id,
+    date: d2
+  })
+end
+
+puts 'Creating workers'
+w1 = Worker.new({
+  name: "Ets CLAIRON",
+  pone: "01 34 45 63 34",
+  email: "contact@clairon.fr",
+  categories: "plomberie, électricité",
+})
+w2 = Worker.new({
+  name: "La CLef D'OR",
+  pone: "01 24 55 93 43",
+  email: "patron@laclefdor.fr",
+  categories: "serrurerie",
+})
+
+puts 'Creating user worker relationship'
+uw1= UserWorker.new({
+  worker_id: w1.id,
+  user_id:rock.id,
+})
+
+uw2= UserWorker.new({
+  worker_id: w2.id,
+  user_id:rock.id,
+})
 
 
 
