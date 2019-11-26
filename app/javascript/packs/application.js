0f308498020f24f036a850b828254900c1edd8de
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-26 15:34:00 +0100

    Add active class to link correspondent to the current page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
+import { activeLink } from '../components/navlink_active_in_navbar';
 import { animateFlashes } from '../components/animate_flashes';
 
+activeLink();
 animateFlashes();
