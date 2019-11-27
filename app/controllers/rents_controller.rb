7152fd39d2164ac7d9c5f688c57fe9eb0aed56b7
Brian Ridolce <brianridolce@gmail.com>
2019-11-27 16:29:25 +0100

    Mise en place de la mécanique de PDF

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -10,6 +10,26 @@ class RentsController < ApplicationController
     end
   end
 
+  def show
+    @rent = Rent.find(params[:id])
+    authorize @rent
+    respond_to do |format|
+      format.html { render :show }
+      format.pdf {
+        render :pdf => "show", :layout => 'pdf.html.erb'
+      }
+    end
+  end
+
+  # def quittance
+  #   respond_to do |format|
+  #     format.html { render :quittance }
+  #     format.pdf {
+  #       render :pdf => "quittance", :layout => 'pdf.html'
+  #     }
+  #   end
+  # end
+
   private
 
   def rent_params
