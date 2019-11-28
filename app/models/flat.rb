bd4b48df5bc10053516de9687bc8a41bbc8388aa
Theodore JUDLIN <theodore.judlin@free.fr>
2019-11-28 14:15:23 +0100

    seed des photos

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,5 @@
 class Flat < ApplicationRecord
+  has_many_attached :photos
   belongs_to :user
   has_many :incidents
   has_many :rentals
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,7 @@ puts 'all records destroyed'
 puts 'Creating some of each...'
 
 puts 'Creating users...'
+puts 'rock'
 rock = User.create!({
   email:  "rf@gmail.com",
   password: "123456",
@@ -35,7 +36,14 @@ rock = User.create!({
   address: "55 rue Singer, 75016 Paris",
   phone: "06 14 14 14 14",
 })
+rock.avatar.attach(
+  io: URI.open('https://upload.wikimedia.org/wikipedia/commons/b/b1/John_D._Rockefeller_Jr._cph.3a03736.jpg'),
+  filename: "#{rock.first_name}_avatar",
+  content_type: 'image/jpg',
+)
 
+
+puts 'alain'
 alain = User.create!({
   email:  "ad@gmail.com",
   password: "123456",
@@ -44,7 +52,13 @@ alain = User.create!({
   address: "33 rue du Laos",
   phone: "06 14 14 14 14",
 })
+alain.avatar.attach(
+  io: URI.open('https://upload.wikimedia.org/wikipedia/commons/a/a9/Delon_Le_Gu%C3%A9pard_%28cropped%29.jpg'),
+  filename: "#{alain.first_name}_avatar",
+  content_type: 'image/jpg',
+)
 
+puts 'robert'
 robert = User.create!({
   email:  "rr@gmail.com",
   password: "123456",
@@ -53,8 +67,15 @@ robert = User.create!({
   address: "10 rue de la Paix",
   phone: "06 12 64 34 22",
 })
+robert.avatar.attach(
+  io: URI.open('https://i.pinimg.com/originals/1c/7b/8b/1c7b8b9962619f064da9ea9fa2633bde.jpg'),
+  filename: "#{robert.first_name}_avatar",
+  content_type: 'image/jpg',
+)
+
 
 puts 'Creating flats...'
+puts 'F1...'
 f1 = Flat.create!({
   user_id: rock.id,
   address: "35 avenue Mac Mahon",
@@ -66,7 +87,43 @@ f1 = Flat.create!({
   syndic_phone: "01 45 43 56 76",
   syndic_email: "contact@foncia.fr",
 })
+f1.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/800/visuels/1/v/z/m/1vzmq8r0i5zzr20jx82vn8e1xzt7cn8q9ztdvexxw.jpg'),
+  filename: "f1_1",
+  content_type: 'image/jpg',
+)
+f1.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/800/visuels/1/b/8/x/1b8xzguqorx3bqtkgruynokk1igwg637eaopcyd04.jpg'),
+  filename: "f1_2",
+  content_type: 'image/jpg',
+)
+f1.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/800/visuels/1/a/u/6/1au6jsmp16igsc9zye3axsv0ru153q1dtgkkzmel0.jpg'),
+  filename: "f1_3",
+  content_type: 'image/jpg',
+)
+f1.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/800/visuels/0/e/6/n/0e6nsmicpooajzbzkr915lykj8l7fsc82td8v7i38.jpg'),
+  filename: "f1_4",
+  content_type: 'image/jpg',
+)
+f1.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/800/visuels/1/u/4/t/1u4tdemiyhva48c36sevxorcdx5pubni0lm0igytw.jpg'),
+  filename: "f1_5",
+  content_type: 'image/jpg',
+)
+f1.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/800/visuels/0/9/e/n/09encdazj2ifehomwr81yub3b6zco3ks6n7f51is4.jpg'),
+  filename: "f1_6",
+  content_type: 'image/jpg',
+)
+f1.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/800/visuels/1/1/l/8/11l85lx2sh47gixtdhugi9kzyps25gid3aml7615g.jpg'),
+  filename: "f1_7",
+  content_type: 'image/jpg',
+)
 
+puts 'F2...'
 f2 = Flat.create!({
   user_id: rock.id,
   address: "35 rue du Loup",
@@ -78,6 +135,49 @@ f2 = Flat.create!({
   syndic_phone: "01 45 43 56 76",
   syndic_email: "contact@foncia.fr",
 })
+f2.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/965/visuels/2/1/r/h/21rhaj90231a5il0cy0sjodlo1bud2hk6zzlzrjsw.jpg'),
+  filename: "f1_2",
+  content_type: 'image/jpg',
+)
+f2.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/965/visuels/0/m/j/1/0mj161xbzgrb3s1z5o2ozaeqx29i1jnrjllqwhssg.jpg'),
+  filename: "f1_3",
+  content_type: 'image/jpg',
+)
+f2.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/965/visuels/1/k/w/7/1kw77vndd6byxoadcngula4casrp8vzfbpx3qu3nk.jpg'),
+  filename: "f1_4",
+  content_type: 'image/jpg',
+)
+f2.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/965/visuels/1/b/9/8/1b98pahcjr1q55y0zp9jgsyc3nhbi437v7mgymu4g.jpg'),
+  filename: "f1_5",
+  content_type: 'image/jpg',
+)
+f2.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/965/visuels/2/9/d/v/29dvogjsd6puqg9vrfqhvlnurb2b52r8fubt7rvnk.jpg'),
+  filename: "f1_6",
+  content_type: 'image/jpg',
+)
+f2.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/966/visuels/1/m/s/h/1msh7k9vcmdzykab8fti2b6eyr00xwjee51w5cq2o.jpg'),
+  filename: "f1_7",
+  content_type: 'image/jpg',
+)
+f2.photos.attach(
+  io: URI.open('https://v.seloger.com/s/width/965/visuels/0/5/6/h/056hrx6qzqkd22ek06itmcu76kl08ky9jg6fu8npc.jpg'),
+  filename: "f1_7",
+  content_type: 'image/jpg',
+)
+
+
+
+
+
+
+
+
 
 puts 'Creating incidents...'
 i1 = Incident.create!({
