b43b7ad6be25b1dbbf5bba4c2e75e4a6a1b1f171
Maxime Chabrol <chabrolmx@gmail.com>
2019-11-28 16:13:48 +0100

    description-out

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -5,6 +5,6 @@ class Worker < ApplicationRecord
   has_many_attached :photos
   validates :name, presence: true, uniqueness: true
   validates :phone, presence: true
-  validates :categories, inclusion: { in:["plomberie", "électricité", "serrurerie", "chauffage", "vitrerie", "petits travaux", "jardinage", "peinture", "sols", "électroménager"]}
+  validates :categories, inclusion: { in: ["plomberie", "électricité", "serrurerie", "chauffage", "vitrerie", "petits travaux", "jardinage", "peinture", "sols", "électroménager"] }
   validates :email, presence: true, format: Devise.email_regexp
 end
